11853f04dab6f4f4c1bd8554de0e4907df927d8f
Tieppo <tieppo.emerson.a.jr@gmail.com>
2026-02-15 17:12:01 -0300

    style: remove svg color duplication adding associations, change color of lib and main

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,21 @@ mod constants;
 
 use crate::constants::{NOT_INCLUDED, RUST_SVG};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::env::home_dir;
 use std::fs;
 use std::fs::File;
 use std::io::Read;
 
-const BASE_DIR: &str = "/Users/tieppo/Library/Application Support/Zed/extensions/installed/material-icon-theme";
+#[cfg(target_os = "macos")]
+const BASE_DIR: &str = "/Library/Application Support/Zed/extensions/installed/material-icon-theme";
+#[cfg(target_os = "linux")]
+const BASE_DIR: &str = "/.local/share/zed/extensions/installed/material-icon-theme";
+
 const ICONS_DIR: &str = "/icons";
 const ICON_REF_DIR: &str = "./icons/";
 const JSON_FILE: &str = "/icon_themes/material-icon-theme.json";
-const TEMP_DIR: &str = "/Users/tieppo/icon-gen";
+const TEMP_DIR: &str = "/gen-icons";
 const SVG_EXTENSION: &str = ".clone.svg";
 const PREFFIXES: &[&str] = &["", ".", "_", "-"];
 const IS_DEBUG: bool = false;
@@ -52,9 +57,11 @@ struct JsonProps {
 }
 
 fn main() -> std::io::Result<()> {
-	let destination = if IS_DEBUG { TEMP_DIR } else { BASE_DIR };
-	let icon_dir = fs::read_dir(format!("{BASE_DIR}{ICONS_DIR}")).expect("[ERROR]: loading directory");
-	let mut json_file = File::open(format!("{BASE_DIR}{JSON_FILE}")).expect("[ERROR]: loading file");
+	let base_dir_to_write = if IS_DEBUG { TEMP_DIR } else { BASE_DIR };
+	let home = home_dir().and_then(|p| p.into_os_string().into_string().ok()).unwrap();
+	let destination = home.clone() + base_dir_to_write;
+	let icon_dir = fs::read_dir(format!("{home}{BASE_DIR}{ICONS_DIR}",)).expect("[ERROR]: loading directory");
+	let mut json_file = File::open(format!("{home}{BASE_DIR}{JSON_FILE}")).expect("[ERROR]: loading file");
 	let mut json_file_content_raw = String::new();
 	let mut theme: JsonProps;
 
@@ -71,7 +78,7 @@ fn main() -> std::io::Result<()> {
 		if f_name.contains("nest") && f_name != "nest.svg" {
 			let mut file_content = String::new();
 
-			File::open(format!("{BASE_DIR}{ICONS_DIR}/{}", &f_name))
+			File::open(format!("{home}{BASE_DIR}{ICONS_DIR}/{}", &f_name))
 				.expect("[ERROR]: opening svg")
 				.read_to_string(&mut file_content)
 				.expect("[ERROR]: reading svg");
@@ -85,17 +92,18 @@ fn main() -> std::io::Result<()> {
 				.next()
 				.expect("[ERROR]: spliting extension, last part");
 
-			write_svg(&f_name, destination, color, &mut theme, extension);
+			write_svg(&f_name, &destination, color, &mut theme, extension, None);
 		}
 	}
 
-	for f in NOT_INCLUDED.into_iter() {
+	for f in NOT_INCLUDED.iter() {
 		write_svg(
 			&format!("rust-{}{SVG_EXTENSION}", f.file_desc_name),
-			destination,
+			&destination,
 			f.color,
 			&mut theme,
 			f.file_desc_name,
+			f.associations,
 		);
 	}
 
@@ -103,25 +111,36 @@ fn main() -> std::io::Result<()> {
 		"{}\n",
 		serde_json::to_string_pretty(&theme).expect("[ERROR]: parsing json to string")
 	);
-	fs::write(format!("{destination}{JSON_FILE}"), &json_file_content_raw).expect("[ERROR]: writing json file");
 
+	fs::write(format!("{destination}{JSON_FILE}"), &json_file_content_raw).expect("[ERROR]: writing json file");
 	Ok(())
 }
 
-fn write_svg(f_name: &str, destination: &str, color: &str, theme: &mut JsonProps, extension: &str) {
+fn write_svg(
+	f_name: &str,
+	destination: &String,
+	color: &str,
+	theme: &mut JsonProps,
+	extension: &str,
+	associations: Option<&'static [&'static str]>,
+) {
 	let new_file_content = RUST_SVG.replace("{{replace_color}}", color);
 	let value = format!("rust-{extension}");
+	let mut file_associations_to_register: HashSet<&str> = associations.unwrap_or(&[]).iter().copied().collect();
+	file_associations_to_register.insert(extension);
 
 	theme.themes[0].file_icons.insert(
-		format!("{value}"),
+		value.clone(),
 		PathValueProps {
 			path: format!("{ICON_REF_DIR}{value}{SVG_EXTENSION}"),
 		},
 	);
 
-	for p in PREFFIXES {
-		theme.themes[0].file_stems.insert(format!("{p}{extension}.rs"), value.clone());
-		theme.themes[0].file_suffixes.insert(format!("{p}{extension}"), value.clone());
+	for e in file_associations_to_register {
+		for p in PREFFIXES {
+			theme.themes[0].file_stems.insert(format!("{p}{e}.rs"), value.clone());
+			theme.themes[0].file_suffixes.insert(format!("{p}{e}"), value.clone());
+		}
 	}
 
 	fs::write(
